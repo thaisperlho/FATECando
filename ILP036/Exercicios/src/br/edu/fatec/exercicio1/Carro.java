c1ab9f1d706aaf4af4b68434d641425602c902ce
thaisperlho <carvalho.thais0301@gmail.com>
2022-08-31 14:27:55 -0300

    update 9 files and create 7 files

--- ILP036/Exercicios/src/br/edu/fatec/exercicio1/Carro.java
+++ ILP036/Exercicios/src/br/edu/fatec/exercicio1/Carro.java
@@ -1,2 +1,67 @@
-package br.edu.fatec.exercicio1;public class Carro {
+package br.edu.fatec.exercicio1;
+
+public class Carro {
+    private String marca;
+    private String cor;
+    private String combustivel;
+    private String modelo;
+    private Integer quilometragem;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(Integer quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public void mostrarDados() {
+        System.out.println("");
+		System.out.println("Marca: " + getMarca());
+        System.out.println("Cor: " + getCor());
+		System.out.println("Tipo de Combustível: " + getCombustivel());
+		System.out.println("Modelo: " + getModelo());
+        System.out.println("Quilometros rodados: " + getQuilometragem() + "km");
+	}
+
+    public void ligar(){
+        System.out.println("Carro ligado!");
+    }
+
+    public void desligar(){
+        System.out.println("Carro desligado!");
+    }
+
 }
--- ILP036/Exercicios/src/br/edu/fatec/exercicio1/Principal.java
+++ ILP036/Exercicios/src/br/edu/fatec/exercicio1/Principal.java
@@ -1,2 +1,37 @@
-package br.edu.fatec.exercicio1;public class Principal {
+package br.edu.fatec.exercicio1;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        int escolha;
+        Carro car = new Carro();
+        Scanner input =  new Scanner(System.in);
+
+        System.out.println("Informe modelo do carro");
+        car.setModelo(input.next());
+        System.out.println("Informe marca");
+        car.setMarca(input.next());
+        System.out.println("Informe cor");
+        car.setCor(input.next());
+        System.out.println("Informe tipo de combustível");
+        car.setCombustivel(input.next());
+        System.out.println("Informe quantidade de quilometros rodados");
+        car.setQuilometragem(input.nextInt());
+        car.mostrarDados();
+
+        System.out.println("");
+        System.out.println("Escolha entre \n 1-Ligar o carro \n 2-Desligar o carro");
+        escolha = input.nextInt();
+        switch(escolha){
+        case 1:
+            car.ligar();
+            break;
+        case 2:
+            car.desligar();
+            break;
+        default:
+            System.out.println("Função inválida!");
+        }
+        input.close();
+    }
 }
