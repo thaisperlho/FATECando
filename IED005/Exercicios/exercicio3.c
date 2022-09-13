f522def728b27ba3f5516b648cb7b65dcb4326d5
thaisperlho <carvalho.thais0301@gmail.com>
2022-09-13 10:56:43 -0300

    feat: create exercicio3 and exercicio3.c

---
+++ IED005/Exercicios/exercicio3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+	float raio, pi=3.14, area;
+	
+	printf("Informe raio:  "); 
+	scanf("%f", &raio);
+ 
+	area = pi * raio * raio;
+ 
+	printf("Area = %.2f m2 \n", area);
+ 
+	return 0;
+}
