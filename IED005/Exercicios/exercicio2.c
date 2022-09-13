29688fb03f892874a2c9af743fdadab1e1ff91a4
thaisperlho <carvalho.thais0301@gmail.com>
2022-09-13 10:50:19 -0300

    update exercicio2 and exercicio2.c

--- IED005/Exercicios/exercicio2.c
+++ IED005/Exercicios/exercicio2.c
@@ -2,14 +2,11 @@
 
 int main(){
 
-  float metros=0, centimetros=0;
-
-  printf("Quantos metros: ");
-  scanf("%f", &metros);
+  float metros=4, centimetros=0;
  
   centimetros = metros * 100;
 
-  printf("Resultado e %.1f cm", centimetros);
+  printf("%.1f mts equivale a %.1f cm",metros, centimetros);
 
   return 0;
 }
