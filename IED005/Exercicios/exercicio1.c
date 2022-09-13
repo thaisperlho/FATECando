fc3c7ce6a80be0122b87b0f5fdc8e06c68b633a6
thaisperlho <carvalho.thais0301@gmail.com>
2022-09-13 10:43:03 -0300

    create 3 files and move and rename 1 file

---
+++ IED005/Exercicios/exercicio1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    printf("informe um número: ");
+    scanf("%i", &numero);
+    printf("O número informado foi %i", numero);
+
+    return 0;
+}
